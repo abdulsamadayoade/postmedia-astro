ab94f387491f56b105be40259159454faa25980b
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2022-08-13 00:05:17 +0100

    add blog post page

---
+++ src/posts/posts.js
@@ -0,0 +1,17 @@
+export const posts = [
+	{
+		id: 1,
+		title: "What Bitcoin Means For Web Security",
+		slug: "what-bitcoin-means-for-web-security",
+	},
+	{
+		id: 2,
+		title: "Security Matters Within Blockchain",
+		slug: "security-matters-within-blockchain",
+	},
+	{
+		id: 3,
+		title: "Why You Need A Wallet For Your Tokens",
+		slug: "why-you-need-a-wallet-for-your-tokens",
+	},
+];
